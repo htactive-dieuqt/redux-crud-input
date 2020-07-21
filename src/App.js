fd208ab55d2d0d5ddeb39e2b386e830cad0e2df0
htactive-dieuqt <dieuqt@htactive.com>
2020-07-21 08:30:34 +0700

    ok

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TransactionList from './components/TransactionList';
+
+function App() {
+  return (
+    <div>
+      <TransactionList />
+    </div>
+  );
+}
+
+export default App;
